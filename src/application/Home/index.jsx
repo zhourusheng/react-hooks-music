f7c3c6be16dd78c605ca92b4f50a9f722efc3100
zhourusheng <nfuzrs@126.com>
2019-12-26 10:34:58 +0800

    home style

--- src/application/Home/index.jsx
+++ src/application/Home/index.jsx
@@ -1,12 +1,36 @@
-import React, {memo} from 'react'
+import React, { memo } from 'react'
 import { renderRoutes } from 'react-router-config'
+import { NavLink } from 'react-router-dom'
+
+import { Top, Tab, TabItem } from './style'
 
 const Home = memo(function Home(props) {
   const { route } = props
-  return(
+  return (
     <>
-    <div>Home</div>
-    {renderRoutes(route.routes)}
+      <Top>
+        <span className='iconfont menu'>&#xe65c;</span>
+        <span className='title'>WebApp</span>
+        <span className='iconfont search'>&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to='/recommend' activeClassName='selected'>
+          <TabItem>
+            <span>推荐</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to='/singers' activeClassName='selected'>
+          <TabItem>
+            <span>歌手</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to='/rank' activeClassName='selected'>
+          <TabItem>
+            <span>排行榜</span>
+          </TabItem>
+        </NavLink>
+      </Tab>
+      {renderRoutes(route.routes)}
     </>
   )
 })
