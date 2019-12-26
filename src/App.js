20b016775736d595ace526338a45e0105217139d
zhourusheng <nfuzrs@126.com>
2019-12-26 10:52:18 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
 
 import routes from './route'
 import { GlobalStyle } from './style'
 import { IconStyle } from './assets/iconfont/iconfont'
+import store from './store'
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle />
-      <IconStyle />
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle />
+        <IconStyle />
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   )
 }
 
